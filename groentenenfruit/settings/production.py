95b872d042c12d03de122ce80f84a4054b388408
Jefwillems <jefwillems100@gmail.com>
2020-01-13 14:21:11 +0100

    update production settings

--- groentenenfruit/settings/production.py
+++ groentenenfruit/settings/production.py
@@ -1,3 +1,5 @@
 from groentenenfruit.settings.base import *
 
+DEBUG = False
+ALLOWED_HOSTS = ['groentenenfruit.herokuapp.com']
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
