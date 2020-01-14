7531fe434fd81d86d1a7aed47c5a1c3e783e7f08
Jefwillems <jefwillems100@gmail.com>
2020-01-14 16:40:51 +0100

    add templates for basic routes

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from shop.views import IndexView
+from shop.views import IndexView, OrderView
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='home')
+    path('', IndexView.as_view(), name='home'),
+    path('order', OrderView.as_view(), name='order')
 ]
--- shop/views.py
+++ shop/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'shop/index.html'
+
+
+class OrderView(TemplateView):
+    template_name = 'shop/order.html'
