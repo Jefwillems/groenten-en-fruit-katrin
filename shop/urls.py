26f89cf7458a9e1b3fbca9eb2728cb8f84a7ec3f
Jefwillems <jefwillems100@gmail.com>
2020-01-14 17:21:44 +0100

    only include media urls in debug mode

--- shop/urls.py
+++ shop/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from shop.views import IndexView, OrderView
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
-                  path('', IndexView.as_view(), name='home'),
-                  path('order', OrderView.as_view(), name='order')
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', IndexView.as_view(), name='home'),
+    path('order', OrderView.as_view(), name='order')
+]
